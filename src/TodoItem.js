71c4308ffe9b8e8e8852dee36c85f948dc955082
jachoi9876@gmail.com <jachoi9876@gmail.com>
2022-10-17 16:51:46 +0900

    TodoItem refactoring ok

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,15 +1,35 @@
-import React from "react";
+// import React from "react";
+
+// const TodoItem = ({ todo, onToggle, onRemove }) => {
+//     const {id, text, done} = todo;
+//     return (
+//     <li>
+//         <span
+//             style={{
+//                 textDecoration: done ? 'line-through': 'none'
+//             }}
+//             onClick={() => onToggle(id)}>{text}</span>
+//         <button onClick ={() => onRemove(id)}>삭제</button>
+//     </li>);
+// };
+
+// export default TodoItem;
+
+import React, { useCallback } from "react";
 
 const TodoItem = ({ todo, onToggle, onRemove }) => {
     const {id, text, done} = todo;
+    const toggle = useCallback(() => onToggle(id), [onToggle, id]);
+    const remove = useCallback(() => onRemove(id), [onRemove, id]);
     return (
     <li>
         <span
             style={{
                 textDecoration: done ? 'line-through': 'none'
-        }}
-        onClick={() => onToggle(id)}>{text}</span>
-        <button onClick ={() => onRemove(id)}>삭제</button>
+            }}
+            // onClick={() => onToggle(id)}>{text}</span>
+            onClick={toggle}>{text}</span>
+        <button onClick ={remove}>삭제</button>
     </li>);
 };
 
