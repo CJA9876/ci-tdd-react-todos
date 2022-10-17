a92e752d622197d94b81ad9e15ebaa8e56abcfba
jachoi9876@gmail.com <jachoi9876@gmail.com>
2022-10-17 17:22:11 +0900

    TodoList test ok

---
+++ src/TodoList.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import TodoList from "./Todolist";
+
+
+describe('<TodoList />', () => {
+    const sampleTodos =[
+        {
+            id: 1,
+            text: 'TDD 배우기',
+            done: false, 
+        },
+        {
+            id: 2,
+            text: 'react-testing-library',
+            done: true, 
+        }
+    ];
+    it('has two todos', () => {
+        const { getByText } = render(<TodoList todos={sampleTodos}/>);
+        getByText(sampleTodos[0].text);
+        getByText(sampleTodos[1].text);
+    });
+});
